6608186a04c19486fa996e7d14ede8db577e3d1e
Aarav Patel <aaravpatel@Aaravs-MacBook-Pro-2.local>
2024-11-02 23:19:53 -0400

    why

--- pages/api/fetchPublications.js
+++ pages/api/fetchPublications.js
@@ -1,8 +1,6 @@
-import fetch from 'node-fetch';
-
 export default async function handler(req, res) {
     const { scholarId } = req.query;
-    const apiKey = "def8dd319f52a47b8a79db5ed7dd96543aa736cbf434f48a80a224a1ebd967ce";
+    const apiKey = process.env.SERPAPI_KEY; // Access the environment variable
     
     if (!scholarId) {
         return res.status(400).json({ error: "Scholar ID is required" });
