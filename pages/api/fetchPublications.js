b2d063e296b63c15db0e40d1fa16bd7728bc8c46
Aarav Patel <aaravpatel@Aaravs-MacBook-Pro-2.local>
2024-11-04 07:22:08 -0500

    sumbmisson

--- pages/api/fetchPublications.js
+++ pages/api/fetchPublications.js
@@ -1,12 +1,11 @@
 
 
 export default async function handler(req, res) {
-    // Set CORS headers to allow requests from your frontend origin
-    res.setHeader('Access-Control-Allow-Origin', '*'); // Use * to allow all origins or specify your frontend URL for more security
+   
+    res.setHeader('Access-Control-Allow-Origin', '*'); 
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 
-    // Handle preflight OPTIONS request
     if (req.method === 'OPTIONS') {
         res.status(200).end();
         return;
@@ -14,7 +13,7 @@ export default async function handler(req, res) {
 
     try {
         const scholarId = req.query.scholarId;
-        const apiKey = process.env.SERPAPI_KEY; // Make sure the environment variable is correctly set in Vercel
+        const apiKey = process.env.SERPAPI_KEY; 
 
         const response = await fetch(`https://serpapi.com/search.json?engine=google_scholar_author&author_id=${scholarId}&api_key=${apiKey}&hl=en`);
         const data = await response.json();
